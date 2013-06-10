e11e2cf8c6c667718e1d019739793378145484a7
chege <chegejames3@gmail.com>
2013-06-11 00:29:49 +0300

    upload

--- app/models/case.rb
+++ app/models/case.rb
@@ -3,8 +3,8 @@ class Case < ActiveRecord::Base
   belongs_to :cause
   attr_accessible :accused, :case_number, :complainant, :court, :defendant, :state, :summary_of_decision, :year_of_judgement
   validates :year_of_judgement, :case_number, :court, :summary_of_decision, presence: true
-  validates :defendant, :complainant, presence: true, :if => :civil_case?
-  validates :state, :accused, presence: true, :if => :criminal_case?
+ # validates :defendant, :complainant, presence: true, :if => :civil_case?
+ # validates :state, :accused, presence: true, :if => :criminal_case?
 
   def civil_case?
     true
