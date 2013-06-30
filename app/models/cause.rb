b8a06ef9367281e86425a899ac90aa664c20c255
chege <chegejames3@gmail.com>
2013-06-30 09:03:00 +0300

    search form

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -3,6 +3,9 @@ class Cause < ActiveRecord::Base
   has_many :cases
 
   CATEGORIES = ["civil", "criminal"]
+  COURTS = ["Milimani Law Courts", "Commercial & Tax Division - Milimani Law Courts", "Busia Law Courts", "Eldoret Law Courts", "Embu Law Courts", "Garissa Law Courts", "Garsen Law Courts", "Githunguri Law Courts", "Hola Law Courts", "Homabay Law Courts", "Kakamega Law Courts", "Kaloleni Law Courts", "Kangundo Law Courts", "Kericho Law Courts", "Kerugoya Law Courts", "Kiambu Law Courts", "Kibera Law Courts", "Kilifi Law Courts", "Kilungu Law Courts", "Kisumu Law Courts", "Kitale Law Courts", "Kwale Law Courts", "Lamu Law Courts", "Lodwar Law Courts", "Machakos Law Courts", "Makadara Law Courts", "Malindi Law Courts", "Mariakani Law Courts", "Mavoko Law Courts", "Meru Law Courts", "Mombasa Law Courts", "Murang'a Law Courts", "Nakuru Law Courts", "Nyeri Law Courts", "Siaya Law Courts", "Tawa Law Courts", "Thika Law Courts", "Tononoka Law Courts", "Ukwala Law Courts", "Voi Law Courts", "Wundanyi Law Courts"]
+  CAUSES = []
+  Cause.all.each{|c| CAUSES << c.name}
 
   validates :category, :description, :name, presence: true
 end
