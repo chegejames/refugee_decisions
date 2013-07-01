8d9e5a8c6f9a1eee0527506e11f62c1434487df4
chege <chegejames3@gmail.com>
2013-07-02 02:43:52 +0300

    check vendor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,7 @@ Unhcrkmja::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  #FIXME was false
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -47,7 +46,7 @@ Unhcrkmja::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( jquery.mobile-1.3.1.min.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
