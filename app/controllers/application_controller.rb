2822540905c39468f7fcb0a023dcdfcc1e36c260
chege <chegejames3@gmail.com>
2013-07-02 10:00:59 +0300

    mobile site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  has_mobile_fu
   before_filter :set_request_format
 
   def set_request_format
-    if is_mobile_devise?
+    if :is_mobile_devise?
       request.format = :mobile
     else
       request.format =:html
     end
   end
-
-  has_mobile_fu
-
-
 
 end
