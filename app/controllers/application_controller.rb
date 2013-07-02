d483ca8a316565c332aa107e9997f00f12dec117
chege <chegejames3@gmail.com>
2013-07-02 09:25:27 +0300

    mobile site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :is_mobile_device?
+
+  before_filter :is_tablet_device?
+
   has_mobile_fu
 
-  before_filter :is_mobile_device?
+
 
 end
