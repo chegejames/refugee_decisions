bc6ec2ae0e678e3aef52c0b520e91fd07721b8ae
chege <chegejames3@gmail.com>
2013-08-08 13:34:14 +0300

    fix judges

--- db/migrate/20130603180232_create_judges.rb
+++ db/migrate/20130603180232_create_judges.rb
@@ -1,9 +1,9 @@
 class CreateJudges < ActiveRecord::Migration
   def change
     create_table :judges do |t|
+      t.integer :membership_id
       t.string :name
       t.string :designation
-      t.integer :membership_id
 
       t.timestamps
     end
